49c534c495605b90662817ee25b55ed7252eb805
Angga <anggadwiarifandi96@gmail.com>
2020-11-26 00:56:13 +0700

    draggable

--- lib/main.dart
+++ lib/main.dart
@@ -14,53 +14,113 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      key: scaffoldKey,
+      appBar: AppBar(),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Draggable(
+              data: 5,
+              child: Container(
+                width: 100.0,
+                height: 100.0,
+                child: Center(
+                  child: Text(
+                    "5",
+                    style: TextStyle(color: Colors.white, fontSize: 22.0),
+                  ),
+                ),
+                color: Colors.pink,
+              ),
+              feedback: Container(
+                width: 100.0,
+                height: 100.0,
+                child: Center(
+                  child: Text(
+                    "5",
+                    style: TextStyle(color: Colors.white, fontSize: 22.0),
+                  ),
+                ),
+                color: Colors.pink,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.green,
+                  child: DragTarget(
+                    builder: (context, List<int> candidateData, rejectedData) {
+                      print(candidateData);
+                      return Center(
+                          child: Text(
+                        "Even",
+                        style: TextStyle(color: Colors.white, fontSize: 22.0),
+                      ));
+                    },
+                    onWillAccept: (data) {
+                      return true;
+                    },
+                    onAccept: (data) {
+                      if (data % 2 == 0) {
+                        scaffoldKey.currentState
+                            .showSnackBar(SnackBar(content: Text("Correct!")));
+                      } else {
+                        scaffoldKey.currentState
+                            .showSnackBar(SnackBar(content: Text("Wrong!")));
+                      }
+                    },
+                  ),
+                ),
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.deepPurple,
+                  child: DragTarget(
+                    builder: (context, List<int> candidateData, rejectedData) {
+                      return Center(
+                          child: Text(
+                        "Odd",
+                        style: TextStyle(color: Colors.white, fontSize: 22.0),
+                      ));
+                    },
+                    onWillAccept: (data) {
+                      return true;
+                    },
+                    onAccept: (data) {
+                      if (data % 2 != 0) {
+                        scaffoldKey.currentState
+                            .showSnackBar(SnackBar(content: Text("Correct!")));
+                      } else {
+                        scaffoldKey.currentState
+                            .showSnackBar(SnackBar(content: Text("Wrong!")));
+                      }
+                    },
+                  ),
+                )
+              ],
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
