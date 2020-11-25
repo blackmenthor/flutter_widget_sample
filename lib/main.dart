9b9ff3357a5034cd6ce8f653e58f8db41171b71f
Angga <anggadwiarifandi96@gmail.com>
2020-11-26 00:59:14 +0700

    listwheel

--- lib/main.dart
+++ lib/main.dart
@@ -39,29 +39,198 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> items = [
+      ListTile(
+        leading: Icon(Icons.local_activity, size: 50),
+        title: Text('Activity'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_airport, size: 50),
+        title: Text('Airport'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_atm, size: 50),
+        title: Text('ATM'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_bar, size: 50),
+        title: Text('Bar'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_cafe, size: 50),
+        title: Text('Cafe'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_car_wash, size: 50),
+        title: Text('Car Wash'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_convenience_store, size: 50),
+        title: Text('Heart Shaker'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_dining, size: 50),
+        title: Text('Dining'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_drink, size: 50),
+        title: Text('Drink'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_florist, size: 50),
+        title: Text('Florist'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_gas_station, size: 50),
+        title: Text('Gas Station'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_grocery_store, size: 50),
+        title: Text('Grocery Store'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_activity, size: 50),
+        title: Text('Activity'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_airport, size: 50),
+        title: Text('Airport'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_atm, size: 50),
+        title: Text('ATM'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_bar, size: 50),
+        title: Text('Bar'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_cafe, size: 50),
+        title: Text('Cafe'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_car_wash, size: 50),
+        title: Text('Car Wash'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_convenience_store, size: 50),
+        title: Text('Heart Shaker'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_dining, size: 50),
+        title: Text('Dining'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_drink, size: 50),
+        title: Text('Drink'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_florist, size: 50),
+        title: Text('Florist'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_gas_station, size: 50),
+        title: Text('Gas Station'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_grocery_store, size: 50),
+        title: Text('Grocery Store'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_activity, size: 50),
+        title: Text('Activity'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_airport, size: 50),
+        title: Text('Airport'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_atm, size: 50),
+        title: Text('ATM'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_bar, size: 50),
+        title: Text('Bar'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_cafe, size: 50),
+        title: Text('Cafe'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_car_wash, size: 50),
+        title: Text('Car Wash'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_convenience_store, size: 50),
+        title: Text('Heart Shaker'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_dining, size: 50),
+        title: Text('Dining'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_drink, size: 50),
+        title: Text('Drink'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_florist, size: 50),
+        title: Text('Florist'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_gas_station, size: 50),
+        title: Text('Gas Station'),
+        subtitle: Text('Description here'),
+      ),
+      ListTile(
+        leading: Icon(Icons.local_grocery_store, size: 50),
+        title: Text('Grocery Store'),
+        subtitle: Text('Description here'),
+      ),
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Center(
+          child: ListWheelScrollView(
+            itemExtent: 75,
+            children: items,
+          ),
+        ));
   }
 }
